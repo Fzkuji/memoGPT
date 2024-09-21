30a22c0aa433cc0c6ba56f312ffb89bf3af9d71f
fzkuj <fzkuji@gmail.com>
2024-09-21 23:01:08 +0800

    Standard the pretrain config.

--- configs/pretrain.py
+++ configs/pretrain.py
@@ -8,14 +8,14 @@ seed = 1337
 
 # 输出和日志
 out_dir = 'out-owt'
-eval_interval = 50
+eval_interval = 200
 eval_iters = 200
 eval_only = False
 
-log_interval = 1
+log_interval = 10
 wandb_log = True  # Feel free to turn on
 wandb_project = 'memoGPT'
-wandb_run_name = 'pretrain-16-256'
+wandb_run_name = 'pretrain-32-64-1024-1024'
 
 # 数据和初始化
 train_mode = 'pretrain'  # pretrain, sft
@@ -25,14 +25,14 @@ elif train_mode == 'pretrain':
     dataset = 'openwebtext'  # fineweb, shakespeare, openwebtext
 
 # 模型初始化
-init_from = 'Qwen/Qwen2-0.5B-Instruct'  # 'Qwen/Qwen2-0.5B-Instruct', 'resume', 'Qwen/Qwen2-7B', 'meta-llama/Llama-2-7b-chat-hf'
+init_from = 'Qwen/Qwen2-1.5B-Instruct'  # 'Qwen/Qwen2-0.5B-Instruct', 'resume', 'Qwen/Qwen2-7B', 'meta-llama/Llama-2-7b-chat-hf'
 
 # 检查点设置
 always_save_checkpoint = False  # Only save checkpoints if the validation loss improves
 
 # 训练参数
-batch_size = 16
-gradient_accumulation_steps = 128
+batch_size = 4
+gradient_accumulation_steps = 32
 max_iters = 60000
 lr_decay_iters = 500
 warmup_iters = 200  # how many steps to warm up for
@@ -54,9 +54,9 @@ long_term_memory_size = ([short_term_memory_size * long_term_memory_chunk_size] 
 bias = True  # Do we use bias inside LayerNorm and Linear layers?
 rms_norm_eps = 1e-06
 input_block_size = 64
-memory_block_size = 16
-train_size_ratio = 16  # 32
-val_size_ratio = 64  # Need 22GB per 1024 * 1024 tokens long context
+memory_block_size = 32
+train_size_ratio = 32  # 32
+val_size_ratio = 32  # Need 22GB per 1024 * 1024 tokens long context
 train_size = memory_block_size * train_size_ratio
 val_size = memory_block_size * val_size_ratio
 
